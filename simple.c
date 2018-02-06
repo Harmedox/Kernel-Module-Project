951d060c4b2f2f17665b86eedd47537ded16f447
Abdulhamid Adebayo <abdulhamid.adebayo@bison.howard.edu>
2018-02-06 00:32:09 -0500

    hash implementation as list

--- simple.c
+++ simple.c
@@ -4,13 +4,16 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/types.h>
-#include <linux/list.h>
+#include <linux/hashtable.h>
 #include <linux/slab.h>
+
+//number of bits defining bucket size
+#define BITS 3
 
 //software license information
 MODULE_LICENSE("GPL");
 //module descripton
-MODULE_DESCRIPTION("Simple Module - Project");
+MODULE_DESCRIPTION("Kernel Module - Project");
 //Module Author
 MODULE_AUTHOR("AAA");
 
@@ -20,10 +23,11 @@ struct Birthday {
 	int day;
 	int month;
 	int year;
-	struct list_head list;
+	struct hlist_node birthday_hash_list;
 };
 
-struct Birthday birthdayList;
+//create hash table 
+DEFINE_HASHTABLE(birthday_hash, BITS);
 
 //define module entry point
 int simple_init(void)
@@ -35,25 +39,28 @@ int simple_init(void)
 	//output message to kernel log buffer. to be read by 'dmesg' command
 	printk(KERN_INFO "Loading Module\n");
 
-	INIT_LIST_HEAD(&birthdayList.list);
-
-	/*adding random elements to the birthday list */
-	for(i=1; i<=5; ++i){
+	/*adding elements to the birthday data structure*/
+	for(i=1; i<=8; ++i){
 		aNewBirthday = kmalloc(sizeof(*aNewBirthday), GFP_KERNEL);
+		//memory availability check
+		if(!aNewBirthday){
+			printk("cannot allocate memory");
+			return -ENOMEM;
+		}
+
 		sprintf(aNewBirthday->name, "user_%d", i);
-		//strcpy(aNewBirthday->name, "user");
 		aNewBirthday->day = 2*i;
 		aNewBirthday->month = i;
 		aNewBirthday->year = 2000 + i;
-		INIT_LIST_HEAD(&aNewBirthday->list);
-		//add item to the birthday list
-		list_add_tail(&(aNewBirthday->list), &(birthdayList.list));
+
+		//add item to the birthday hash
+		hash_add(birthday_hash, &aNewBirthday->birthday_hash_list, aNewBirthday->name);
 	}
 	
-	printk(KERN_INFO "Traversing birthday list \n");
-	list_for_each_entry(aBirthday, &birthdayList.list, list) {
-		//print entries of the list, each record as object aBirthday
-		printk(KERN_INFO "Name: %s; Day: %d; Month: %d; Year: %d",aBirthday->name, aBirthday->day, aBirthday->month, aBirthday->year);
+	printk(KERN_INFO "Traversing birthday hash \n");
+	hash_for_each(birthday_hash, i, aBirthday, birthday_hash_list) {
+		//print entries of the hash table, each record as object aBirthday
+		printk(KERN_INFO "Entry[%d]: Name: %s, Day: %d, Month: %d, Year: %d\n", i,aBirthday->name,aBirthday->day,aBirthday->month,aBirthday->year);
 	}
 
 	printk(KERN_INFO "\n");
@@ -64,17 +71,20 @@ int simple_init(void)
 //define module exit point
 void simple_exit(void)
 {
-	struct Birthday *aBirthday, *tmp;
+	struct Birthday *aBirthday;
+	unsigned int j; //node pointer
+
 	printk(KERN_INFO "Removing Module\n");
 
-	printk(KERN_INFO "delete records in the birthday list");
+	printk(KERN_INFO "delete nodes in the birthday hash table");
 
-	list_for_each_entry_safe(aBirthday, tmp, &birthdayList.list, list){
-	//delete each object of birthday record
-	printk(KERN_INFO "node deleted for %s", aBirthday->name);
-	list_del(&aBirthday->list);
-	kfree(aBirthday);
+	hash_for_each(birthday_hash, j, aBirthday, birthday_hash_list){
+		//delete each node of birthday hash table
+		printk(KERN_INFO "Entry[%d]: deleted", j);
+		hash_del(&aBirthday->birthday_hash_list);
+		kfree(aBirthday);
 	}
+	printk(KERN_INFO "\n");
 }
 
 //register module entry and exit points with kernel
